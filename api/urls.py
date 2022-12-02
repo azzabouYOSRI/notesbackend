d4c67b55e933c8d738accc8a64032fd49555037c
yosriazabou <yosriazabou@fss.u-sfax.tn>
2022-12-02 10:56:06 +0100

    bugs fixed

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
-    path('notes/', views.get_notes),
-    path('notes/<str:pk>/', views.get_note),
-    path('notes/', views.create_note),
-    path('notes/update/<str:pk>/', views.update_note),
+    path('notes', views.get_notes),
+    path('notes1/', views.create_note),
+    path('notes/<str:pk>/', views.update_note),
     path('notes/delete/<str:pk>/', views.delete_note),
 ]
